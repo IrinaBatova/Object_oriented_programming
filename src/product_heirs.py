0778d1276da04f1dc2047b3fca628c2c55ad69de
Irina Batova <BatIr_74_2@mail.ru>
2026-05-23 20:54:09 +0300

    A new "Smartphone" class has been created in the src/product_heirs.py module.

---
+++ src/product_heirs.py
@@ -0,0 +1,37 @@
+from src.product import Product
+
+# 16.1 Наследование. Задание_1.
+
+
+class Smartphone(Product):
+    """
+    Smartphone ("Смартфон") класс наследник класса Product
+    """
+
+    # Атрибуты (поля) класса. Определены на уровне класса, общие для всех экземпляров (объектов) класса.
+    # Пока их нет.
+
+    # Указываем типы атрибутов экземпляров класса — это часть аннотации типов,
+    # какие типы данных ожидаются для каждого атрибута экземпляра класса
+    efficiency: str  # Производительность
+    model: str  # Модель
+    memory: float  # Объем встроенной памяти
+
+    # Переопределяем магический метод __init__ базового класса
+    def __init__(
+        self, name, description, price, quantity, color, efficiency, model, memory
+    ):
+
+        # Вызываем метод __init__ базового класса
+        super().__init__(name, description, price, quantity)
+
+        # Дополнительный код новые атрибуты (поля) экземпляра (объекта) класса Smartphone
+        self.efficiency = efficiency  # Производительность
+        self.model = model  # Модель
+        self.memory = memory  # Объем встроенной памяти
+        self.color = color # Цвет
+
+if __name__ == '__main__':
+    smartphone1 = Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, "Серый",
+                             "256GB", "S23 Ultra", 95.5)
+    print(smartphone1)
