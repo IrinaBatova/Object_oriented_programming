cfd5c12b7c611a55a614d16926b86b7c9697b475
Irina Batova <BatIr_74_2@mail.ru>
2026-05-28 21:20:46 +0300

    Added a test for testing the ReprMixin class (mixin).

---
+++ tests/test_mixins.py
@@ -0,0 +1,24 @@
+from src.mixins import ReprMixin
+
+# Вспомогательный (мок) класс для тестирования миксина
+class User(ReprMixin):
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+        # Обеспечивает вызов следующего класса в MRO
+        super().__init__()
+
+
+class TestReprMixin:
+
+    def test_repr_mixin(self):
+
+        # Подготовка данных
+        test_obj = User("Alice", 30)
+
+        # Вызов тестируемого метода миксина
+        result = test_obj.__repr__()
+
+        assert result == "User(name='Alice', age=30)"
